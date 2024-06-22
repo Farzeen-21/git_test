d2e69a979924a75c18b0981f848ed0c52e175834
Farzeen Bano Sayyed <farzeen.s@intellectbizware.com>
2024-06-22 19:24:28 +0000

    testing

---
+++ webapp/controller/Master.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.ibspl.gittest.controller.Master", {
+        onInit: function () {
+            //local_git_test
+        }
+    });
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"comibspl/git_test/test/unit/controller/Master.controller"
+], function () {
+	"use strict";
+});
